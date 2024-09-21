79da20eb0bbba1ef0f17f0045e050db770797314
Jon Luntzel <34121312+jonluntzel@users.noreply.github.com>
2024-09-22 06:25:13 +0700

    Delete zle_src directory
    was going to normalize source directories across projects, but don't want to mess up the test files

--- zle_src/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present Abraham Miller <abraham.r.m@gmail.com>
-#
-# SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
--- zle_src/__init__.py
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present Abraham Miller <abraham.r.m@gmail.com>
-#
-# SPDX-License-Identifier: MIT
-
-from .eigs import eigs
-from . import gen
-from . import fgen
